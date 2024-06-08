17c0f2c9af315a290d1f0a8acd3b8ccf54c90acd
nkoorty <amalyshau2002@gmail.com>
2024-06-08 19:09:22 +0200

    added frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,18 @@
+import { Search } from './search';
+
+export default async function IndexPage({
+  searchParams
+}: {
+  searchParams: { q: string; offset: string };
+}) {
+  const search = searchParams.q ?? '';
+  const offset = searchParams.offset ?? 0;
+
+  return (
+    <main className="flex flex-1 flex-col p-4 md:p-6">
+      <div className="flex items-center mb-8">
+        <h1 className="font-semibold text-lg md:text-2xl">App</h1>
+      </div>
+    </main>
+  );
+}
