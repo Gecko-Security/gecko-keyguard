24f580388c642fbf53b8fe57dc4ff42b15bd78c9
acse-am9023 <amalyshau2002@gmail.com>
2024-08-15 23:06:29 +0100

    fix: footer

--- frontend/app/ui/dashboard/footer/footer.jsx
+++ frontend/app/ui/dashboard/footer/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className={styles.container}>
             <div className={styles.logo}>DKMI</div>
-            <div className={styles.text}>XRPL x EasyA Hackathon</div>
+            <div className={styles.text}>XRPL</div>
         </div>
     )
 }
